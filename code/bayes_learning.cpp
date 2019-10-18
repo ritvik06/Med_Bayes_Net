fb7e732d83afebbc55c03fb4fddff5c0703bfbf1
Navneel Mandal <navneel99@gmail.com>
2019-10-18 23:21:13 +0530

    Code Restructured.

---
+++ code/bayes_learning.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include<bits/stdc++.h>
+
+
+// int main(int argc, char const *argv[]) {
+//
+//
+//
+//
+//   return 0;
+// }
---
+++ headers/comm_headers.h
@@ -0,0 +1,13 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <list>
+#include <fstream>
+#include <sstream>
+#include <cstdlib>
+
+#endif
