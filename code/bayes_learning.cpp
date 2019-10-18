f643eaa0cbcc6180525466fdbb6e22af5e05c090
Navneel Mandal <navneel99@gmail.com>
2019-10-19 00:35:10 +0530

    Probability Initializer Created.

--- code/bayes_learning.cpp
+++ code/bayes_learning.cpp
@@ -1,11 +1,60 @@
-#include <iostream>
-#include<bits/stdc++.h>
+#include "bayes_learning.h"
+
+vector<float> initialize_probability(network n, vector<vector<int> > db){
+  list<Graph_Node> g_l = n.Pres_Graph;
+  list <Graph_Node>::iterator it;
+  int ind=0;
+  for (it = g_l.begin();it!=g_l.end();it++){
+    Graph_Node curr_node = *(n.get_nth_node(ind));
+    vector<float> curr_table = curr_node.get_CPT();
+    if (curr_node.get_Parents().size()!=0){ //If not a starting node
+      float filled=0;
+      int num=0;
+      for (int i = 0; i< curr_table.size();i++){
+        if (curr_table[i]>=0){
+          filled+=curr_table[i];
+        }else{
+          num++;
+        }
+      }
+      float left = (1-filled)/num;
+      for (int i = 0; i< curr_table.size();i++){
+        if (curr_table[i]<0){
+          curr_table[i] = left;
+        }
+    }
+    curr_node.set_CPT(curr_table);
+  }else{
+    int n = db.size();
+    int m = db[0].size();
+    int all_v = curr_node.get_nvalues();
+    vector<float> freq(all_v,0);
+    int all_sum=0;
+    for (int i = 0; i< n;i++){
+      int val = db[i][ind];
+      if (val >= 0){
+        freq[val]++;
+        all_sum++;
+      }
+    }
+    for (int i = 0; i<all_v;i++){
+      freq[i]/=all_sum;
+    }
+    curr_node.set_CPT(freq);
+  }
+  ind++;
+}
+}
 
 
-// int main(int argc, char const *argv[]) {
-//
-//
-//
-//
-//   return 0;
-// }
+
+
+int main(int argc, char const *argv[]) {
+    network Alarm;
+    Alarm = read_network();
+
+    // Graph_Node g = *(Alarm.get_nth_node(0));
+
+    // cout<<it.begin()<<" "<<it++<<" "<<endl;
+  return 0;
+}
---
+++ headers/bayes_learning.h
@@ -0,0 +1,7 @@
+#ifndef BAYES_LEARNING_H
+#define BAYES_LEARNING_H
+#include "startup_code.h"
+
+
+
+#endif
